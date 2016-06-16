0784d25eca4207fb6ad46fa4bce2b4e102bd6b23
Chris Fallin <cfallin@c1f.net>
2016-06-16 17:33:46 -0400

    WIP.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,32 @@
-// 
+// immutable_arena: a Rust crate for arenas of immutable-once-built objects
+// with references to other objects in the arena.
+//
+// Copyright (c) Chris Fallin <cfallin@c1f.net>. Released under the MIT
+// license.
+
 extern crate typed_arena;
+extern crate smallvec;
 
 use std::fmt;
 use std::mem;
 use std::marker::PhantomData;
 use std::ops::{Deref, DerefMut};
+use std::sync::Mutex;
 use std::sync::atomic::{AtomicPtr, Ordering};
+use smallvec::SmallVec;
 
+/// An `Arena<T>` is a container of objects of type `T` that, once allocated,
+/// live as long as the containing arena. Within the arena, objects may refer
+/// to other objects using the `Ref<'arena, T>` smart-pointer type. These
+/// object references are allowed to form cycles. The only restriction is that
+/// once created, an object is immutable. Objects are created within a "builder
+/// context", via the `build()` method on the `Arena<T>`. Within the builder
+/// context, user code has mutable access to objects, and queue reference
+/// assignments to / other objects.
+///
+/// *Note:* the builder interface allows safe code to obtain a simultaneous
+/// mutable and immutable borrow of an instance of `T` by setting a `Ref` and
+/// immediately dereferencing it while still holding the `BuilderRef`.
 pub struct Arena<T> {
     arena: typed_arena::Arena<T>,
 }
@@ -19,7 +39,10 @@ impl<T> Arena<T> {
     pub fn build<'arena, F, R>(&'arena self, build: F) -> R
         where F: FnOnce(&mut Builder<'arena, T>) -> R
     {
-        let mut builder = Builder { arena: self };
+        let mut builder = Builder {
+            arena: self,
+            assignments: Mutex::new(SmallVec::new()),
+        };
         build(&mut builder)
     }
 }
@@ -28,15 +51,80 @@ pub struct Builder<'arena, T>
     where T: 'arena
 {
     arena: &'arena Arena<T>,
+    assignments: Mutex<SmallVec<[RefAssignment<'arena, T>; 4]>>,
 }
 
-impl<'arena, T> Builder<'arena, T> {
-    pub fn new(&mut self, t: T) -> Ref<'arena, T> {
-        let obj: &'arena mut T = self.arena.arena.alloc(t);
+struct RefAssignment<'arena, T> {
+    from: *const Ref<'arena, T>,
+    to: *const T,
+}
+
+impl<'builder, 'arena, T> Builder<'arena, T>
+    where 'arena: 'builder,
+          T: 'arena
+{
+    pub fn new(&'builder mut self, t: T) -> BuilderRef<'builder, 'arena, T> {
+        BuilderRef {
+            ptr: self.arena.arena.alloc(t),
+            builder: self,
+        }
+    }
+
+    pub fn freeze(&'builder self, t: BuilderRef<'builder, 'arena, T>) -> Ref<'arena, T> {
         Ref {
-            ptr: AtomicPtr::new(obj as *mut T),
+            ptr: AtomicPtr::new(t.ptr as *mut T),
             _lifetime: PhantomData,
         }
+    }
+}
+
+impl<'arena, T> Drop for Builder<'arena, T>
+    where T: 'arena
+{
+    fn drop(&mut self) {
+        for r in self.assignments.lock().unwrap().iter() {
+            let from: &Ref<'arena, T> = unsafe { mem::transmute(r.from) };
+            if from.ptr.compare_and_swap(0 as *mut T, r.to as *mut T, Ordering::Relaxed) !=
+               0 as *mut T {
+                panic!("Attempt to re-set a Ref that has already been set.");
+            }
+        }
+    }
+}
+
+pub struct BuilderRef<'builder, 'arena, T>
+    where 'arena: 'builder,
+          T: 'arena
+{
+    ptr: &'arena mut T,
+    builder: &'builder Builder<'arena, T>,
+}
+
+impl<'builder, 'arena, T> Deref for BuilderRef<'builder, 'arena, T>
+    where 'arena: 'builder,
+          T: 'arena
+{
+    type Target = T;
+    fn deref(&self) -> &T {
+        self.ptr
+    }
+}
+
+impl<'builder, 'arena, T> DerefMut for BuilderRef<'builder, 'arena, T>
+    where 'arena: 'builder,
+          T: 'arena
+{
+    fn deref_mut(&mut self) -> &mut T {
+        self.ptr
+    }
+}
+
+impl<'builder, 'arena, T> fmt::Debug for BuilderRef<'builder, 'arena, T>
+    where 'arena: 'builder,
+          T: 'arena + fmt::Debug
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.deref().fmt(f)
     }
 }
 
@@ -55,12 +143,13 @@ impl<'arena, T> Ref<'arena, T>
         }
     }
 
-    pub fn set(&self, pointee: &Ref<'arena, T>) {
-        if self.ptr.compare_and_swap(0 as *mut T,
-                                     pointee.ptr.load(Ordering::Relaxed),
-                                     Ordering::Relaxed) != 0 as *mut T {
-            panic!("Attempt to re-set a Ref that has already been set.");
-        }
+    pub fn set<'builder>(&'arena self, to: &BuilderRef<'builder, 'arena, T>)
+        where 'arena: 'builder
+    {
+        to.builder.assignments.lock().unwrap().push(RefAssignment {
+            from: self as *const Ref<'arena, T>,
+            to: self.ptr.load(Ordering::Relaxed) as *mut T,
+        });
     }
 }
 
@@ -69,14 +158,6 @@ impl<'arena, T> Deref for Ref<'arena, T>
 {
     type Target = T;
     fn deref(&self) -> &T {
-        unsafe { mem::transmute(self.ptr.load(Ordering::Relaxed)) }
-    }
-}
-
-impl<'arena, T> DerefMut for Ref<'arena, T>
-    where T: 'arena
-{
-    fn deref_mut(&mut self) -> &mut T {
         unsafe { mem::transmute(self.ptr.load(Ordering::Relaxed)) }
     }
 }
@@ -135,7 +216,7 @@ mod test {
             y.b.set(&z);
             z.a.set(&x);
             z.b.set(&y);
-            (x, y, z)
+            (ctx.freeze(x), ctx.freeze(y), ctx.freeze(z))
         });
         assert!(x.a.id == 1);
         assert!(x.b.id == 2);
