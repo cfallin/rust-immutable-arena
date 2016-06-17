f4b866b1f563b7cd3d29bcd077d28db92bd00875
Chris Fallin <cfallin@c1f.net>
2016-06-16 21:00:30 -0400

    Initial commit of docs.

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["immutable_arena"] = ["impl&lt;'arena,&nbsp;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='immutable_arena/struct.Ref.html' title='immutable_arena::Ref'>Ref</a>&lt;'arena,&nbsp;T&gt; <span class='where'>where T: 'arena + <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["immutable_arena"] = ["impl&lt;'arena,&nbsp;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='immutable_arena/struct.Ref.html' title='immutable_arena::Ref'>Ref</a>&lt;'arena,&nbsp;T&gt; <span class='where'>where T: 'arena</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
